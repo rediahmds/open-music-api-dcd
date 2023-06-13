8b2b550f64fa1b0a1997fce6f8de2c2e51bf6988
Redi Ahmad <redi.ngoding@gmail.com>
2023-06-14 01:08:55 +0700

    feat(routes): :sparkles: define routes on `/songs/` endpoint

---
+++ src/apis/songs/routes.js
@@ -0,0 +1,29 @@
+const routes = (handlers) => [
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: (request, h) => handlers.postSongHandler(request, h),
+  },
+  {
+    method: 'GET',
+    path: '/songs',
+    handler: () => handlers.getSongsHandler(),
+  },
+  {
+    method: 'GET',
+    path: '/songs/{id}',
+    handler: (request, h) => handlers.getSongByIdHandler(request, h),
+  },
+  {
+    method: 'PUT',
+    path: '/songs/{id}',
+    handler: (request, h) => handlers.putSongByIdHandler(request, h),
+  },
+  {
+    method: 'DELETE',
+    path: '/songs/{id}',
+    handler: (request, h) => handlers.deleteSongByIdHandler(request, h),
+  },
+];
+
+module.exports = routes;
