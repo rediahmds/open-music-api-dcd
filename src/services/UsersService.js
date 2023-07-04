97d9ed9b82da6b483127bc171a425b88171774cc
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-05 01:46:45 +0700

    feat(service): :sparkles: created `UsersService` to serve auth

---
+++ src/services/UsersService.js
@@ -0,0 +1,63 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../exceptions/InvariantError');
+const NotFoundError = require('../exceptions/NotFoundError');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    await this.validateUsernameUniqueness(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const insertUserQuery = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(insertUserQuery);
+
+    if (!result.rowCount) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async validateUsernameUniqueness(username) {
+    const usernameQuery = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(usernameQuery);
+
+    if (result.rowCount) {
+      throw new InvariantError(
+        'Gagal menambahkan user. Username telah digunakan'
+      );
+    }
+  }
+
+  async getUserById(userId) {
+    const findUserByIdQuery = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+
+    const result = await this._pool.query(findUserByIdQuery);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
+  }
+}
+
+module.exports = UsersService;
