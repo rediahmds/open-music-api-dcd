89aaf7e49e408efe8b72c389ec2e30b871c609ac
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-08 01:20:06 +0700

    refactor(validator): :recycle: constraints changes to adjust postman tests

--- src/validators/authentications/schema.js
+++ src/validators/authentications/schema.js
@@ -2,15 +2,15 @@ const Joi = require('joi');
 
 const PostAuthenticationPayloadSchema = Joi.object({
   username: Joi.string().min(3).max(50).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(3).required(), // min(3) menyesuaikan dengan test case
 });
 
 const PutAuthenticationPayloadSchema = Joi.object({
-  refreshToken: Joi.string().base64({ urlSafe: true }).required(),
+  refreshToken: Joi.string().min(1).regex(/^[A-Za-z0-9+/=_.-]+$/).required(),
 });
 
 const DeleteAuthenticationPayloadSchema = Joi.object({
-  refreshToken: Joi.string().base64({ urlSafe: true }).required(),
+  refreshToken: Joi.string().min(1).regex(/^[A-Za-z0-9+/=_.-]+$/).required(),
 });
 
 module.exports = {
