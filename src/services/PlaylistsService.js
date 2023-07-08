d2c70664e81521d6af85f382254ca88562510a53
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-08 23:02:08 +0700

    feat(service): :sparkles: defines `PlaylistsService`

---
+++ src/services/PlaylistsService.js
@@ -0,0 +1,72 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../exceptions/InvariantError');
+const NotFoundError = require('../exceptions/NotFoundError');
+const AuthorizationError = require('../exceptions/AuthorizationError');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addPlaylist(playlistName, owner) {
+    const id = `playlist-${nanoid(16)}`;
+    const addPlaylistQuery = {
+      text: 'INSERT INTO playlists VALUES($1, $2, $3) RETURNING id',
+      values: [id, playlistName, owner],
+    };
+
+    const result = await this._pool.query(addPlaylistQuery);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Playlist gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getPlaylists(owner) {
+    const getAllOwnerPlaylistsQuery = {
+      text: 'SELECT * FROM playlists WHERE owner = $1',
+      values: [owner],
+    };
+
+    const result = await this._pool.query(getAllOwnerPlaylistsQuery);
+
+    return result.rows;
+  }
+
+  async deletePlaylistById(playlistId) {
+    const deleteOwnerPlaylistQuery = {
+      text: 'DELETE FROM playlists WHERE id = $1 RETURNING id',
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(deleteOwnerPlaylistQuery);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Gagal menghapus. Playlist tak ditemukan');
+    }
+  }
+
+  async verifyPlaylistOwnership(playlistId, owner) {
+    const getPlaylistAndOwnerQuery = {
+      text: 'SELECT owner FROM playlists WHERE id = $1',
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(getPlaylistAndOwnerQuery);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Playlist tidak ditemukan');
+    }
+
+    const playlist = result.rows[0];
+
+    if (playlist.owner !== owner) {
+      throw new AuthorizationError('Anda tidak memiliki akses ke resource ini');
+    }
+  }
+}
+
+module.exports = PlaylistsService;
