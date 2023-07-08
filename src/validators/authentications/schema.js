9609fb395331d870db34893578c46036bd731ea9
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-08 13:30:58 +0700

    refactor(jwt): :recycle: changes token validation regex to match base64-url format

--- src/validators/authentications/schema.js
+++ src/validators/authentications/schema.js
@@ -1,16 +1,19 @@
 const Joi = require('joi');
 
+// token base64-URL harus mengikuti pola ini
+const base64UrlRegex = /^[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+){2}$/;
+
 const PostAuthenticationPayloadSchema = Joi.object({
   username: Joi.string().min(3).max(50).required(),
   password: Joi.string().min(3).required(), // min(3) menyesuaikan dengan test case
 });
 
 const PutAuthenticationPayloadSchema = Joi.object({
-  refreshToken: Joi.string().min(1).regex(/^[A-Za-z0-9+/=_.-]+$/).required(),
+  refreshToken: Joi.string().min(1).regex(base64UrlRegex).required(),
 });
 
 const DeleteAuthenticationPayloadSchema = Joi.object({
-  refreshToken: Joi.string().min(1).regex(/^[A-Za-z0-9+/=_.-]+$/).required(),
+  refreshToken: Joi.string().min(1).regex(base64UrlRegex).required(),
 });
 
 module.exports = {
