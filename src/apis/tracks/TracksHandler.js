9259da52ff14db846f5fb1c11cd9fc01590441c6
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-09 23:29:13 +0700

    feat(service): :sparkles: defines `TracksService` to handle operation on playlist_songs table

---
+++ src/apis/tracks/TracksHandler.js
@@ -0,0 +1,71 @@
+class TracksHandler {
+  constructor(tracksService, playlistsService, tracksValidator) {
+    this._tracksService = tracksService;
+    this._playlistsService = playlistsService;
+    this._tracksValidator = tracksValidator;
+  }
+
+  async postTrackHandler(request, h) {
+    this._tracksValidator.validateTrackPayload(request.payload);
+
+    const { id: playlistId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._tracksService.verifyPlaylistAccess(playlistId, credentialId);
+
+    const { songId } = request.payload;
+
+    await this._tracksService.addTrack(playlistId, songId);
+
+    return h
+      .response({
+        status: 'success',
+        message: 'Lagu berhasil ditambahkan ke playlist',
+      })
+      .code(201);
+  }
+
+  async getTracksHandler(request, h) {
+    const { id: playlistId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._tracksService.verifyPlaylistAccess(playlistId, credentialId);
+
+    const tracksInPlaylist = await this._tracksService.getTracksInPlaylist(
+      playlistId
+    );
+    const playlistDetails = await this._playlistsService.getPlaylistDetailsById(
+      playlistId
+    );
+
+    return h.response({
+      status: 'success',
+      data: {
+        playlist: {
+          ...playlistDetails,
+          songs: tracksInPlaylist,
+        },
+      },
+    });
+  }
+
+  async deleteTrackHandler(request, h) {
+    this._tracksValidator.validateTrackPayload(request.payload);
+
+    const { id: playlistId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._tracksService.verifyPlaylistAccess(playlistId, credentialId);
+
+    const { id: songId } = request.payload;
+
+    await this._tracksService.deleteTrackById(playlistId, songId);
+
+    return h.response({
+      status: 'success',
+      message: 'Berhasil menghapus lagu dari playlist',
+    });
+  }
+}
+
+module.exports = TracksHandler;
