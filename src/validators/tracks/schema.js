88a2d22df5dad017eea9af8c57bedd596dbbe2eb
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-17 10:40:39 +0700

    chore(validator): :hammer: changes in `TracksValidator` to adhere postman tests

--- src/validators/tracks/schema.js
+++ src/validators/tracks/schema.js
@@ -1,11 +1,7 @@
 const Joi = require('joi');
 
 const TrackPayloadSchema = Joi.object({
-  songId: Joi.string()
-    .min(5)
-    .max(50)
-    .regex(/song-.+/)
-    .required(),
+  songId: Joi.string().min(3).max(50).required(),
 });
 
 module.exports = TrackPayloadSchema;
