11b39f3c1c52d56bd169e43d3db60d3918dbe088
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-20 15:43:58 +0700

    feat(db migration): :sparkles: create new table, "collaborations"

---
+++ migrations/1689840508449_create-collaborations-table.js
@@ -0,0 +1,30 @@
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'playlists(id)',
+      onDelete: 'cascade',
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'cascade',
+    },
+  });
+
+  pgm.addConstraint(
+    'collaborations',
+    'unique_playlist_id_and_user_id',
+    'UNIQUE(playlist_id, user_id)'
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations');
+};
