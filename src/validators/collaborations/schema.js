d3b97d39a940593456e92797ea60021a5b5706d6
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-25 13:00:41 +0700

    feat(validator): :sparkles: add max character validation

--- src/validators/collaborations/schema.js
+++ src/validators/collaborations/schema.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
 const CollaborationPayloadSchema = Joi.object({
-  playlistId: Joi.string().min(3).required(),
-  userId: Joi.string().min(3).required(),
+  playlistId: Joi.string().min(3).max(50).required(),
+  userId: Joi.string().min(3).max(50).required(),
 });
 
 module.exports = { CollaborationPayloadSchema };
