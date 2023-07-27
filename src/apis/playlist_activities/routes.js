314599d182facc2ae8f3043804ecd2e320e22701
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-27 22:22:44 +0700

    fix(api): :bug: incorrect path

--- src/apis/playlist_activities/routes.js
+++ src/apis/playlist_activities/routes.js
@@ -1,7 +1,7 @@
 const routes = (handlers) => [
   {
     method: 'GET',
-    path: 'playlists/{id}/activities',
+    path: '/playlists/{id}/activities',
     handler: (request, h) => handlers.getActivitiesHandler(request, h),
     options: {
       auth: 'openmusic_jwt',
