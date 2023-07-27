f22c62757014e63935db3624ec5458e4480206a7
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-27 21:55:05 +0700

    feat(apis): :sparkles: provide endpoint to access playlist activities history

---
+++ src/apis/playlist_activities/PlaylistActivitiesHandler.js
@@ -0,0 +1,28 @@
+class PlaylistActivitiesHandler {
+  constructor(playlistActivitiesService, playlistsService) {
+    this._playlistActivitiesService = playlistActivitiesService;
+    this._playlistsService = playlistsService;
+  }
+
+  async getActivitiesHandler(request, h) {
+    const { id: playlistId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._playlistsService.verifyPlaylistAccess(playlistId, credentialId);
+
+    const playlistActivities = await this._playlistActivitiesService
+      .getPlaylistActivitiesByPlaylistId(playlistId);
+
+    console.log(playlistActivities);
+
+    return h.response({
+      status: 'success',
+      message: 'Berhasil mendapatkan daftar aktivitas playlist',
+      data: {
+        playlistActivities,
+      },
+    });
+  }
+}
+
+module.exports = PlaylistActivitiesHandler;
---
+++ src/apis/playlist_activities/routes.js
@@ -0,0 +1,12 @@
+const routes = (handlers) => [
+  {
+    method: 'GET',
+    path: 'playlists/{id}/activities',
+    handler: (request, h) => handlers.getActivitiesHandler(request, h),
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+];
+
+module.exports = routes;
