2ec55479f43710dc177405a19b65ac758b798a37
Redi Ahmad <redi.ngoding@gmail.com>
2023-07-28 17:25:39 +0700

    refactor(schemas): :recycle: improves payload schema criteria

--- src/validators/albums/schema.js
+++ src/validators/albums/schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const currentYear = new Date().getFullYear();
 const AlbumPayloadSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().min(3).token().required(),
   year: Joi.number().min(1900).max(currentYear).required(),
 });
 
